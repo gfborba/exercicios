215bba4d37e4cecd6ed20780e4b5a70ae4b89d14
Guilherme Borba <guifborba@hotmail.com>
2022-06-20 14:32:11 -0300

    Create exe03.sql

---
+++ MySQL/exe03.sql
@@ -0,0 +1,10 @@
+insert into pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+('Nathaly', '2001-09-11', 'F', '60.8', '1.30', 'Espanha'),
+('Maicon', '1999-02-01', 'M', '100.3', '2.70', 'Dinamarca'),
+('Sergio', '2007-05-01', 'F', '30.2', '3.00', 'Albânia'); 
+
+select * from pessoas;
+
+
