a8e3083272fc893eebd515f363bea63f0e0c6f97
Guilherme Borba <guifborba@hotmail.com>
2022-07-04 16:42:52 -0300

    Create exe09 (select pt 1).sql

---
+++ MySQL/exe09 (select pt 1).sql
@@ -0,0 +1,11 @@
+select * from gafanhotos;
+
+select * from cursos
+order by nome;
+
+select ano, nome, carga from cursos
+order by ano, nome;
+
+select nome, carga from cursos
+where ano = '2016'
+order by nome;
