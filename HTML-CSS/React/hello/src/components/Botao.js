df25fc351af85d891241c564f9f7756f3a88492b
Gui <guifborba@hotmail.com>
2024-11-08 20:17:10 -0300

    primeiros passos em react

---
+++ HTML-CSS/React/hello/src/components/Botao.js
@@ -0,0 +1,14 @@
+function Botao(props){
+    return(
+        <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {props.titulo}
+      </a>
+    )
+}
+
+export default Botao;
